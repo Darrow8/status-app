14d5a4b805be14462552b0d03235759b2b62746c
Darrow H <darrowrhartman@gmail.com>
2020-05-05 11:14:46 -0700

    improved Post

--- status-app/src/app/Post.ts
+++ status-app/src/app/Post.ts
@@ -9,9 +9,31 @@ export class Post {
      * @param timebomb the timer that will delete this post
      * @param type the type of post this is
      */
+    name: string;
+    id: string;
+    user: User;
+    timebomb: Date;
+    type: PostType;
+
     constructor(name: string, id: string, user: User, 
-        timebomb: Date,type: PostType,nickname: string,) {
-        
+        timebomb: Date, type: PostType) {
+        this.name = name,
+        this.id = id,
+        this.user = user,
+        this.timebomb = timebomb,
+        this.type = type
+    }
+    //* For debugging lots of stuff
+    returnInfo(){
+        console.log("---------------------")
+        console.log("Post Log of " + this.id)
+        console.log("Post's name " + this.name)
+        console.log("Post's id " + this.id)
+        console.log("Post's user " + this.user)
+        console.log("Post's timebomb date: " + this.timebomb)
+        console.log("Post's type is: " + this.type)
+        console.log("---------------------")
+
     }
     selfDestruct(){
         //TODO self destructs
